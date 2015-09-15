202ccc6318d0472f46202dc5fa0cc7ae6f0f38aa
Cody Jenkins <iknowcss@gmail.com>
2015-09-15 21:15:07 +1000

    Enhanced test

--- test/component/inputselectSpec.js
+++ test/component/inputselectSpec.js
@@ -1,7 +1,21 @@
+import React from 'react/addons';
+import TestUtils from 'react/lib/ReactTestUtils';
+
 import InputSelect from '../../src/js/component/inputselect';
 
 describe('inputselect Component', () => {
-  it('runs', () => {
-    
+  let element;
+
+  before(() => {
+    element = TestUtils.renderIntoDocument(
+      <InputSelect
+        name="testname"
+      />
+    );
+  });
+
+  it('renders', () => {
+    let select = TestUtils.findRenderedDOMComponentWithTag(element, 'select');
+    expect(select.props.name).to.be('');
   });
 });
