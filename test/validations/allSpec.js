2919d3c4dab382b8236bc64db21c931942259d28
CodyJenkins <cody.jenkins@btfinancialgroup.com>
2015-09-18 14:45:12 +1000

    Whitespace

--- test/validations/allSpec.js
+++ test/validations/allSpec.js
@@ -3,12 +3,12 @@ import get from 'lodash/object/get';
 
 function xvalidate() {}
 
-function validate(path, cases, isOnly) {
+function validate(path, tests, isOnly) {
   const sut = get(all, path);
   const desc = isOnly ? describe.only : describe;
 
   desc(path, () => {
-    (cases || []).forEach(test => {
+    (tests || []).forEach(test => {
       if (test.hasOwnProperty('validation')) {
         let expected = test.validation;
         it(`"${test.testCase}" validation result "${expected}"`, () => {
@@ -19,49 +19,50 @@ function validate(path, cases, isOnly) {
   });
 }
 
-validate.only = function (path, cases) {
-  return validate(path, cases, true);
+validate.only = function (path, tests) {
+  return validate(path, tests, true);
 };
 
 describe.only('validation', () => {
-    validate('telephone.change.customFormat', [
-      { validation: true, testCase: '' },
-      { validation: true, testCase: ' ()+' },
-      { validation: true, testCase: ' ()+ 0123456789' },
-      { validation: true, testCase: ' ()+ 012345678' },
-      { validation: true, testCase: ' ()+ 0' },
-      { validation: true, testCase: ' ()+ 123456789012345' },
-      { validation: true, testCase: ' ()+ 1' },
-      { validation: false, testCase: '#' },
-      { validation: false, testCase: ' ()+ 01234567890' },
-      { validation: false, testCase: '0#' },
-      { validation: false, testCase: ' ()+ 1234567890123456' },
-      { validation: false, testCase: '1#' },
-    ]);
+  validate('telephone.change.customFormat', [
+    { validation: true, testCase: '' },
+    { validation: true, testCase: ' ()+' },
+    { validation: true, testCase: ' ()+ 0123456789' },
+    { validation: true, testCase: ' ()+ 012345678' },
+    { validation: true, testCase: ' ()+ 0' },
+    { validation: true, testCase: ' ()+ 123456789012345' },
+    { validation: true, testCase: ' ()+ 1' },
+    { validation: false, testCase: '#' },
+    { validation: false, testCase: ' ()+ 01234567890' },
+    { validation: false, testCase: '0#' },
+    { validation: false, testCase: ' ()+ 1234567890123456' },
+    { validation: false, testCase: '1#' },
+  ]);
 
-    validate('telephone.blur.customFormat', [
-      { validation: true, testCase: ' ()+0551000000' },
-      { validation: true, testCase: ' ()+61' },
-      { validation: true, testCase: ' ()+6155' },
-      { validation: true, testCase: ' ()+61551' },
-      { validation: true, testCase: ' ()+615510000000000' },
-      { validation: true, testCase: ' ()+1' },
-      { validation: true, testCase: ' ()+123456789012345' },
-      { validation: false, testCase: ' ' },
-      { validation: false, testCase: '#' },
-      { validation: false, testCase: ' ()+ 01234567890' },
-      { validation: false, testCase: '0#' },
-      { validation: false, testCase: ' ()+ 1234567890123456' },
-      { validation: false, testCase: '1#' },
-      { validation: false, testCase: ' ()+' },
-      { validation: false, testCase: ' ()+05510000000' },
-      { validation: false, testCase: ' ()+055100000' },
-      { validation: false, testCase: ' ()+0550000000' },
-      { validation: false, testCase: ' ()+61550' },
-      { validation: false, testCase: ' ()+615500000000000' },
-      { validation: false, testCase: ' ()+6155100000000000' },
-      { validation: false, testCase: ' ()+1234567890123456' },
-      { validation: null, testCase: undefined },
-      { validation: null, testCase: '' },
-    ]);
+  validate('telephone.blur.customFormat', [
+    { validation: true, testCase: ' ()+0551000000' },
+    { validation: true, testCase: ' ()+61' },
+    { validation: true, testCase: ' ()+6155' },
+    { validation: true, testCase: ' ()+61551' },
+    { validation: true, testCase: ' ()+615510000000000' },
+    { validation: true, testCase: ' ()+1' },
+    { validation: true, testCase: ' ()+123456789012345' },
+    { validation: false, testCase: ' ' },
+    { validation: false, testCase: '#' },
+    { validation: false, testCase: ' ()+ 01234567890' },
+    { validation: false, testCase: '0#' },
+    { validation: false, testCase: ' ()+ 1234567890123456' },
+    { validation: false, testCase: '1#' },
+    { validation: false, testCase: ' ()+' },
+    { validation: false, testCase: ' ()+05510000000' },
+    { validation: false, testCase: ' ()+055100000' },
+    { validation: false, testCase: ' ()+0550000000' },
+    { validation: false, testCase: ' ()+61550' },
+    { validation: false, testCase: ' ()+615500000000000' },
+    { validation: false, testCase: ' ()+6155100000000000' },
+    { validation: false, testCase: ' ()+1234567890123456' },
+    { validation: null, testCase: undefined },
+    { validation: null, testCase: '' },
+  ]);
+
 });
