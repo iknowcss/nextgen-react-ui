be87299fe9e1abb326db8642c6064bdade5582dd
CodyJenkins <cody.jenkins@btfinancialgroup.com>
2015-09-23 12:05:27 +1000

    Move component tests to js sub-directory

--- test/js/component/inputselectSpec.js
+++ test/js/component/inputselectSpec.js
@@ -1,7 +1,7 @@
 import React from 'react/addons';
 import TestUtils from 'react/lib/ReactTestUtils';
 
-import InputSelect from '../../src/js/component/inputselect';
+import InputSelect from '../../../src/js/component/inputselect';
 
 describe('inputselect Component', () => {
   let element;
