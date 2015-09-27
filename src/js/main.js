1f95e638c6f24e89ce30214bda4af073a4b1a6c5
Cody Jenkins <iknowcss@gmail.com>
2015-09-27 19:02:25 +1000

    Remove dblclick handler from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,4 @@
 import React from 'react/addons';
 import DemoLoader from './demo/demoloader';
 
-const element = <DemoLoader/>;
-
-React.render(
-  element,
-  document.getElementById('app-container')
-);
-
-document.body.ondblclick = function () {
-  console.log(React.renderToString(element));
-};
+React.render(<DemoLoader/>, document.getElementById('app-container'));
