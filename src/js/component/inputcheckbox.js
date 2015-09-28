7048acadaac6d1598441c20850eac974cc426dae
CodyJenkins <cody.jenkins@btfinancialgroup.com>
2015-09-28 12:31:30 +1000

    inputcheckbox base

---
+++ src/js/component/inputcheckbox.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react/addons';
+
+export default class InputCheckbox extends Component {
+    render() {
+        return <div></div>;
+    }
+}
---
+++ test/js/component/inputcheckboxSpec.js
@@ -0,0 +1,12 @@
+import React from 'react/addons';
+import TestUtils from 'react/lib/ReactTestUtils';
+
+import InputCheckbox from '../../../src/js/component/inputcheckbox';
+
+describe.only('inputcheckbox Component', () => {
+    let element;
+
+    it('renders', () => {
+
+    });
+});
